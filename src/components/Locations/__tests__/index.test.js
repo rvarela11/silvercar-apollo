b263ce7703c871ad5b00f2efcbda5975c2117d20
Roberto  Varela <rvarela@Robertos-MacBook-Pro.local>
2018-11-18 11:56:06 -0600

    Updated components tests

---
+++ src/components/Locations/__tests__/index.test.js
@@ -0,0 +1,17 @@
+// @vendors
+import React from 'react';
+import { shallow } from 'enzyme';
+
+// @components
+import Locations from '../Locations';
+
+describe('Locations Container', () => {
+    it('renders Locations without crashing', () => {
+        shallow(
+            <Locations
+                data={[]}
+                handleOnChangeSearch={() => {}}
+            />
+        );
+    });
+});
