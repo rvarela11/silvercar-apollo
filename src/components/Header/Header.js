88db42067daf88cb64620eac6a29773bbec27f7b
Roberto  Varela <rvarela@Robertos-MacBook-Pro.local>
2018-11-19 12:19:38 -0600

    Updating header styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,7 @@
 // @vendors
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,22 +11,35 @@ import Toolbar from '@material-ui/core/Toolbar';
 // @styles
 import './Header.scss';
 
-const Header = () => (
-    <div className="header">
-        <AppBar color="inherit" position="static">
-            <Toolbar>
-                <IconButton className="menuButton" color="inherit" aria-label="Menu">
-                    <MenuIcon />
-                </IconButton>
-                <figure className="header__logo" />
-                <div className="header__buttons">
-                    <Button className="header__button" color="inherit" disableRipple>SIGN UP</Button>
-                    <Button className="header__button" color="inherit" disableRipple>LOG IN</Button>
-                </div>
-                <Button className="header__button header__help-button" color="inherit" disableRipple>Help</Button>
-            </Toolbar>
-        </AppBar>
-    </div>
-);
+const styles = {
+    headerAppBar: {
+        boxShadow: 'none'
+    }
+};
 
-export default Header;
+const Header = (props) => {
+    const { classes } = props;
+    return (
+        <div className="header">
+            <AppBar className={classes.headerAppBar} color="inherit" position="static">
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <figure className="header__logo" />
+                    <div className="header__buttons">
+                        <Button className="header__button" color="inherit" disableRipple>SIGN UP</Button>
+                        <Button className="header__button" color="inherit" disableRipple>LOG IN</Button>
+                    </div>
+                    <Button className="header__button header__help-button" color="inherit" disableRipple>Help</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+};
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
